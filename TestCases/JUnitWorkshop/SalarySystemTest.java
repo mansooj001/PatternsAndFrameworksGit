36173539ae8b6bd7fa8a00e042010a6b33771b5b
Jason Mansoor <jason.mansoor@gmail.com>
2016-12-19 10:56:05 +0100

    JUnit Workshop toegevoegd en voorbereid.

---
+++ TestCases/JUnitWorkshop/SalarySystemTest.java
@@ -0,0 +1,42 @@
+package JUnitWorkshop;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jason on 12/15/2016.
+ */
+public class SalarySystemTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void addColleague() throws Exception {
+        
+    }
+
+    @Test
+    public void search() throws Exception {
+
+    }
+
+    @Test
+    public void raiseSalary() throws Exception {
+
+    }
+
+    @Test
+    public void printAll() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
